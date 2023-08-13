1c7e386865d3dac1b7b8e0ef6faafe1bb3b16305
Burak <90563983+burakgulirmak35@users.noreply.github.com>
2023-08-13 14:55:35 +0300

    ++

--- Assets/Dev/Script/UIManager.cs
+++ Assets/Dev/Script/UIManager.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance { get; private set; }
-
-    [SerializeField] private TextMeshProUGUI txt_Message;
+    [Space]
+    [SerializeField] private TextMeshProUGUI txtMessage;
+    [Space]
     [SerializeField] private Button btn_Rotate;
+    [SerializeField] private Button btn_Restart;
+    [Header("GameOver")]
+    [SerializeField] private GameObject panelGameOver;
+    [Space]
+    [SerializeField] private TextMeshProUGUI txt_Winner;
+    [Space]
+    [SerializeField] private TextMeshProUGUI txt_p1shotCount;
+    [SerializeField] private TextMeshProUGUI txt_p1hitCount;
+    [Space]
+    [SerializeField] private TextMeshProUGUI txt_time;
 
     public event EventHandler Rotate;
 
@@ -18,16 +30,30 @@ public class UIManager : MonoBehaviour
     {
         Instance = this;
         btn_Rotate.onClick.AddListener(BtnRotate);
+        btn_Restart.onClick.AddListener(BtnRestart);
     }
 
     public void MessageText(string _message)
     {
-        txt_Message.text = _message;
+        txtMessage.text = _message;
         Debug.Log(_message);
+    }
+
+    private void EndGame(string _winner)
+    {
+        panelGameOver.SetActive(true);
+        txt_Winner.text = _winner + "win";
     }
 
     private void BtnRotate()
     {
         Rotate?.Invoke(this, EventArgs.Empty);
     }
+
+    private void BtnRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
 }
